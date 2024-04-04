a18a6e12793d0d3982bc0794b195cbb993861ef7
0oAVIRALo0 <aviral21029@iiitd.ac.in>
2024-04-04 16:34:35 +0530

    paytm without razorpay

---
+++ frontend/src/pages/Signin.jsx
@@ -0,0 +1,39 @@
+import { BottomWarning } from "../components/BottomWarning";
+import { Button } from "../components/Button";
+import { Heading } from "../components/Heading";
+import { InputBox } from "../components/InputBox";
+import { SubHeading } from "../components/SubHeading";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const Signin = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      navigate("/dashboard");
+    }
+  }, [navigate]);
+
+  return (
+    <div className="bg-slate-300 h-screen flex justify-center">
+      <div className="flex flex-col justify-center">
+        <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+          <Heading label={"Sign in"} />
+          <SubHeading label={"Enter your credentials to access your account"} />
+          <InputBox placeholder="example@example.com" label={"Email"} />
+          <InputBox placeholder="password" label={"Password"} />
+          <div className="pt-4">
+            <Button label={"Sign in"} />
+          </div>
+          <BottomWarning
+            label={"Don't have an account?"}
+            buttonText={"Sign up"}
+            to={"/signup"}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
